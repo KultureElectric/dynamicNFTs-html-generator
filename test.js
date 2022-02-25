1cbc5a15dab2666524a1f500e13c364936cf9c6d
Ruben Schaller <rubenusshd@gmail.com>
2022-02-25 17:22:53 +0200

    test2

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 const test = () => {
     console.log('test');
-    const response = fetch('https://github.com/KultureElectric/NFTMakerPatch/blob/main/build/index.js', {
+    const response = fetch('https://github.com/KultureElectric/dynamicNFTs-html-generator/blob/main/metadata.js', {
                     method : "GET",
                     cache: 'no-cache'
                     }).then(response => response.json());
